516a427426aa40b6d4c60c69f1f6f5e0b1527b86
K13abcz <148306813+K13abcz@users.noreply.github.com>
2025-05-13 15:30:05 +0900

    Update xgboost.py

--- result/classifier performance evaluation/xgboost.py
+++ result/classifier performance evaluation/xgboost.py
@@ -3,6 +3,7 @@ import numpy as np
 from gensim.models.doc2vec import Doc2Vec
 from xgboost import XGBClassifier as xgb
 from sklearn.metrics import f1_score
+from logistic_regression.py import addvec
 
 def evaluate_category_xgboost(category, X_train, X_test, train_df, test_df):
     """
