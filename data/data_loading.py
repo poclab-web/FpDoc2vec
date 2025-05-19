a142ddd6be8063d02b7192a040f68f9a054d15af
K13abcz <148306813+K13abcz@users.noreply.github.com>
2025-05-19 15:41:47 +0900

    Update data_loading.py

--- data/data_loading.py
+++ data/data_loading.py
@@ -1,15 +1,25 @@
 import pickle
 import time
 import requests
-from tqdm import tqdm
-from bs4 import BeautifulSoup
+from typing import Dict, List, Optional, Tuple, Union
+import pubchempy as pcp
 from rdkit import Chem
 from rdkit.Chem import PandasTools
-import pubchempy as pcp
+from bs4 import BeautifulSoup
+from tqdm import tqdm
 import pandas as pd
 
-def smiles_to_cid(smiles, return_single=True):
-    """Convert SMILES to PubChem CID"""
+
+def smiles_to_cid(smiles: str, return_single: bool = True) -> Union[int, List[int], None]:
+    """Convert SMILES to PubChem CID
+    
+    Args:
+        smiles: String containing the SMILES notation of a molecule
+        return_single: Boolean indicating whether to return a single CID (True) or a list of CIDs (False)
+        
+    Returns:
+        A single CID (integer), a list of CIDs, or None if an error occurs
+    """
     try:
         compounds = pcp.get_compounds(smiles, "smiles")
         cids = list(map(lambda x: x.cid, compounds))
@@ -19,9 +29,17 @@ def smiles_to_cid(smiles, return_single=True):
         return cids[0]
     else:
         return cids
+
+
+def fetch_compound_descriptions(df: pd.DataFrame) -> Dict[Tuple[str, str, str], str]:
+    """Fetch descriptions from PubChem using various identifiers
+    
+    Args:
+        df: DataFrame containing compound information with columns 'inchikey', 'cid', 'smiles', and 'NAME'
         
-def fetch_compound_descriptions(df):
-    """Fetch descriptions from PubChem using various identifiers"""
+    Returns:
+        Dictionary with keys as tuples (NAME, smiles, inchikey) and values as compound descriptions
+    """
     descriptions = {}
     temp_array1 = df['inchikey'].tolist()
     temp_array2 = df['cid'].tolist()
@@ -44,8 +62,16 @@ def fetch_compound_descriptions(df):
                 continue
     return descriptions
 
-def mol_to_inchikey(mol_list):
-    """Convert RDKit molecule objects to InChIKeys"""
+
+def mol_to_inchikey(mol_list: List) -> List[Union[str, int]]:
+    """Convert RDKit molecule objects to InChIKeys
+    
+    Args:
+        mol_list: List of RDKit molecule objects
+        
+    Returns:
+        List of InChIKeys as strings, with 0 for molecules that couldn't be converted
+    """
     inchikeys = []
     for i in mol_list:
         try:
@@ -54,7 +80,17 @@ def mol_to_inchikey(mol_list):
             inchikeys.append(0)
     return inchikeys
 
-def main(input_sdf_file, output_file_name):
+
+def main(sdf_file: str, file_name: str) -> None:
+    """Process SDF file to extract compound information and fetch descriptions from PubChem
+    
+    Args:
+        sdf_file: Path to the SDF file containing chemical compound data
+        file_name: Path where the output pickle file will be saved
+        
+    Returns:
+        None
+    """
     # Load chemical data
     df = PandasTools.LoadSDF(sdf_file)
     data_df = df[["ChEBI Name", "ROMol"]].rename(columns={"ChEBI Name":"NAME"})
@@ -103,5 +139,6 @@ def main(input_sdf_file, output_file_name):
     with open(file_name, "wb") as f:
         pickle.dump(all1, f)
 
+
 if __name__ == "__main__":
-    main(input_sdf_file, output_file_name)
+    main(sdf_file="path/to/your/file.sdf", file_name="output_descriptions.pkl")
