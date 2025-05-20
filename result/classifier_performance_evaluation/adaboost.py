2fb18fa0f18c8e3dc8a70aa222b8b2fef5458d97
K13abcz <148306813+K13abcz@users.noreply.github.com>
2025-05-21 04:30:20 +0900

    Update adaboost_params.py

--- result/classifier_performance_evaluation/adaboost.py
+++ result/classifier_performance_evaluation/adaboost.py
@@ -1,9 +1,14 @@
+from typing import Dict, Any
 
-    dt = DecisionTreeClassifier(max_depth=2, class_weight="balanced")
-    ada = AdaBoostClassifier(
-        estimator=dt,
-        n_estimators=442,
-        learning_rate=0.07760379807069998,
-        algorithm="SAMME.R",
-        random_state=50
-    )
+dt_params: Dict[str, Any] = {
+    "max_depth": 2, 
+    "class_weight": "balanced"
+}
+
+ada_params: Dict[str, Any] = {
+    "estimator": dt,
+    "n_estimators": 442,
+    "learning_rate": 0.07760379807069998,
+    "algorithm": "SAMME.R",
+    "random_state": 50
+}
