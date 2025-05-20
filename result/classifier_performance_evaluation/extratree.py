a8c00df3aae4d3e20602f5e19954b3f38638ed68
K13abcz <148306813+K13abcz@users.noreply.github.com>
2025-05-21 04:23:52 +0900

    Update extratree.py

--- result/classifier_performance_evaluation/extratree.py
+++ result/classifier_performance_evaluation/extratree.py
@@ -1,11 +1,14 @@
-et_params = {n_estimators=672, 
-        criterion="log_loss", 
-        max_depth=15, 
-        min_samples_split=29, 
-        min_samples_leaf=1, 
-        min_weight_fraction_leaf=0.0030605268601344767, 
-        max_features=None, 
-        bootstrap=False, 
-        class_weight="balanced_subsample", 
-        random_state=50
+from typing import Dict, Any
+
+et_params: Dict[str, Any] = {
+        "n_estimators": 672, 
+        "criterion": "log_loss", 
+        "max_depth": 15, 
+        "min_samples_split": 29, 
+        "min_samples_leaf": 1, 
+        "min_weight_fraction_leaf": 0.0030605268601344767, 
+        "max_features": None, 
+        "bootstrap": False, 
+        "class_weight": "balanced_subsample", 
+        "random_state": 50
 }
