7d393d013b7c54419a06a6b92f0f15732c4dd84e
K13abcz <148306813+K13abcz@users.noreply.github.com>
2025-05-21 04:30:56 +0900

    Update main.py

--- result/classifier_performance_evaluation/main.py
+++ result/classifier_performance_evaluation/main.py
@@ -43,3 +43,23 @@ def main(traindf_path, testdf_path, model_path):
 
 if __name__ == "__main__":
     main()
+
+
+(
+        boosting_type="dart", 
+        n_estimators=444, 
+        learning_rate=0.07284380689492893, 
+        max_depth=6, 
+        num_leaves=41, 
+        min_child_samples=21, 
+        class_weight="balanced", 
+        reg_alpha=1.4922729949843299, 
+        reg_lambda=2.8809246344115778, 
+        colsample_bytree=0.5789063337359206, 
+        subsample=0.5230422589468584, 
+        subsample_freq=2, 
+        drop_rate=0.1675163179873052, 
+        skip_drop=0.49103811434109507, 
+        objective='binary', 
+        random_state=50
+    )
