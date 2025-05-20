e0d7331adc66c76b8699762a1fb47fa9c53117eb
K13abcz <148306813+K13abcz@users.noreply.github.com>
2025-05-21 02:12:09 +0900

    Update NameDoc2Vec.py

--- result/full_prediction/NameDoc2Vec.py
+++ result/full_prediction/NameDoc2Vec.py
@@ -1,11 +1,7 @@
-import pickle
 import numpy as np
-from rdkit.Chem import AllChem
-from gensim.models.doc2vec import Doc2Vec, TaggedDocument
-import lightgbm as lgb
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import f1_score
-from FpDoc2vec.py import evaluate_category, create_lightgbm_classifier
+import pandas as pd
+from gensim.models.doc2vec import Doc2Vec
+
 
 def make_name2vector(model_path: str, df: pd.DataFrame) -> np.ndarray:
     """Convert to compound vectors using NameDoc2Vec model
@@ -21,32 +17,3 @@ def make_name2vector(model_path: str, df: pd.DataFrame) -> np.ndarray:
     vec = np.array([model.dv.vectors[i] for i in range(len(df))])
     return vec
     
-    
-def main():
-    # Load dataset
-    with open("../../data/10genre_dataset.pkl", "rb") as f:
-        df = pickle.load(f)
-    
-    # Define categories to evaluate
-    categories = [
-        'antioxidant', 'anti_inflammatory_agent', 'allergen', 'dye', 'toxin', 
-        'flavouring_agent', 'agrochemical', 'volatile_oil', 'antibacterial_agent', 'insecticide'
-    ]
-    
-    # loading Doc2Vec model
-    model = Doc2Vec.load("../../model/namedoc2vec.model")
-    
-    # Generate compound vectors
-    X_vec = np.array([model.dv.vectors[i] for i in range(len(df))])
-    
-    # Create classifier
-    lightgbm_model = create_lightgbm_classifier()
-    
-    # Evaluate each category
-    results = {}
-    for category in categories:
-        y = np.array([1 if i == category else 0 for i in df[category]])
-        results[category] = evaluate_category(category, X_vec, y, lightgbm_model)
-    
-if __name__ == "__main__":
-    main()
