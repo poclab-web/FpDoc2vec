3e998177d24aef5a3f4ba776b69a84930f87d605
K13abcz <148306813+K13abcz@users.noreply.github.com>
2025-05-21 06:01:21 +0900

    Update shapvalue_to_structure_ecfp.py.py

--- result/SHAP/shapvalue_to_structure_ecfp.py.py
+++ result/SHAP/shapvalue_to_structure_ecfp.py.py
@@ -197,11 +197,4 @@ if __name__ == "__main__":
     chemical_data_path = "10genre_dataset.pkl"
     target_molecule = "quercetin" # Please modify according to the purpose.
     
-    main(
-        shap_values_path=shap_values_path,
-        chemical_data_path=chemical_data_path,
-        target_molecule=target_molecule,
-        fp_radius=3,
-        nBits=4096,
-        scale_factor=1.0
-    )
+    main(shap_values_path, chemical_data_path=chemical_data_path, target_molecule, fp_radius=3, nBits=4096, scale_factor=1.0)
