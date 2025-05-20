12ee1f991a94c961bd7c72a0d7085be56785e7d3
K13abcz <148306813+K13abcz@users.noreply.github.com>
2025-05-21 02:00:22 +0900

    Update 32Descriptors.py

--- result/full_prediction/32Descriptors.py
+++ result/full_prediction/32Descriptors.py
@@ -7,6 +7,14 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import f1_score
 from FpDoc2vec.py import evaluate_category, create_lightgbm_classifier
 
+def make_descriptor(input_path):
+    # Load dataset
+    with open(input_path, "rb") as f:
+        df = pickle.load(f)
+    # Generate compound vectors
+    desc = np.array(df.iloc[:, 14:])
+    return desc
+    
 def main():
     # Load dataset
     with open("../../10genre_32descriptor.pkl", "rb") as f:
