d76e90c02276cfe081b6699ac360700a9fd6a898
K13abcz <148306813+K13abcz@users.noreply.github.com>
2025-05-21 04:36:27 +0900

    Update main.py

--- result/classifier_performance_evaluation/main.py
+++ result/classifier_performance_evaluation/main.py
@@ -1,3 +1,5 @@
+from sklearn.linear_model import LogisticRegression
+
 def main(traindf_path, testdf_path, model_path):
     """
     Main function to load data, prepare features, and evaluate models
@@ -30,9 +32,10 @@ def main(traindf_path, testdf_path, model_path):
     # Evaluate each category
     train, test = [], []
     for category in categories:
-        train_score, test_score = evaluate_category(
-            category, X_train_vec, X_test_vec, train_df, test_df
-        )
+        train_score, test_score = evaluate_category(category, 
+X_vec: np.ndarray, 
+                      y: np.ndarray, 
+                      estimator_model
         train.append(train_score)
         test.append(test_score)
     
@@ -42,24 +45,6 @@ def main(traindf_path, testdf_path, model_path):
     print(f"Average Test F1: {np.mean(test):.4f}")
 
 if __name__ == "__main__":
+    #LR
+    LogisticRegression
     main()
-
-
-(
-        boosting_type="dart", 
-        n_estimators=444, 
-        learning_rate=0.07284380689492893, 
-        max_depth=6, 
-        num_leaves=41, 
-        min_child_samples=21, 
-        class_weight="balanced", 
-        reg_alpha=1.4922729949843299, 
-        reg_lambda=2.8809246344115778, 
-        colsample_bytree=0.5789063337359206, 
-        subsample=0.5230422589468584, 
-        subsample_freq=2, 
-        drop_rate=0.1675163179873052, 
-        skip_drop=0.49103811434109507, 
-        objective='binary', 
-        random_state=50
-    )
