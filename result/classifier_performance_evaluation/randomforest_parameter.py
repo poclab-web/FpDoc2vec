53695c46a3c9b7437ac8b858afd8366dd019503e
K13abcz <148306813+K13abcz@users.noreply.github.com>
2025-05-21 04:25:16 +0900

    Update randomforest_parameter.py

--- result/classifier_performance_evaluation/randomforest_parameter.py
+++ result/classifier_performance_evaluation/randomforest_parameter.py
@@ -1,12 +1,15 @@
-rf_params = {n_estimators=1075, 
-        criterion="gini", 
-        max_depth=16, 
-        min_samples_split=30, 
-        min_samples_leaf=8, 
-        min_weight_fraction_leaf=0.011003051241804895, 
-        max_features="sqrt", 
-        bootstrap=True, 
-        class_weight="balanced", 
-        max_samples=0.6800609792930714, 
-        random_state=50
+from typing import Dict, Any
+
+rf_params: Dict[str, Any] = {
+       "n_estimators": 1075, 
+       "criterion": "gini", 
+       "max_depth": 16, 
+       "min_samples_split": 30, 
+       "min_samples_leaf": 8, 
+       "min_weight_fraction_leaf": 0.011003051241804895, 
+       "max_features": "sqrt", 
+       "bootstrap": True, 
+       "class_weight": "balanced", 
+       "max_samples": 0.6800609792930714, 
+       "random_state": 50
 }
