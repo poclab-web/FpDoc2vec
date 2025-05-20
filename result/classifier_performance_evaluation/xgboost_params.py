bf608fe1ef7fdb3528116f3b6e546038f88a9050
K13abcz <148306813+K13abcz@users.noreply.github.com>
2025-05-21 04:26:09 +0900

    Update and rename xgboost.py to xgboost_params.py

---
+++ result/classifier_performance_evaluation/xgboost_params.py
@@ -0,0 +1,17 @@
+pythonfrom typing import Dict, Any
+
+xgb_params: Dict[str, Any] = {
+    "n_estimators": 375, 
+    "max_depth": 8,
+    "learning_rate": 0.036865217123129936, 
+    "booster": "gbtree",
+    "gamma": 0.9396457047650646, 
+    "min_child_weight": 3.567611062612357,
+    "max_delta_step": 4.897284214673892, 
+    "subsample": 0.54928595002729,
+    "colsample_bytree": 0.7402540955402481, 
+    "reg_alpha": 1,
+    "reg_lambda": 3, 
+    "scale_pos_weight": 9.305137370400152, 
+    "random_state": 50
+}
