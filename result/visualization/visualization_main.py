85d8c001bb200f20538cc79e7dbdf4b6b07300f4
K13abcz <148306813+K13abcz@users.noreply.github.com>
2025-05-21 15:12:16 +0900

    Update visualization_main.py

--- result/visualization/visualization_main.py
+++ result/visualization/visualization_main.py
@@ -1,22 +1,21 @@
 from typing import Dict
-
+import pandas as pd
+import numpy as np
 from visualization_function import load_data, add_vectors, generate_umap_embedding, plot_chemical_categories, make_name2vector, make_fp2vector, main
 
 
-if __name__ == "__main__":
-    
-    #FpDoc2vec visualization
-    input_path: str = "10genre_dataset.pkl"
-    model_path: str = "fpdoc2vec.model"
-    output_path: str = "fpdoc2vec_umap.png"
-    df: pd.DataFrame = load_data(input_path)
-    vec: Dict[str, np.ndarray] = make_fp2vector(model_path, df)
-    main(input_path, vec, output_path)
-    
-    #NameDoc2vec visualization
-    input_path: str = "10genre_dataset.pkl"
-    model_path: str = "namedoc2vec.model"
-    output_path: str = "namedoc2vec_umap.png"
-    df: pd.DataFrame = load_data(input_path)
-    vec: Dict[str, np.ndarray] = make_name2vector(model_path, df)
-    main(input_path, vec, output_path)
+#FpDoc2vec visualization
+input_path: str = "10genre_dataset.pkl"
+model_path: str = "fpdoc2vec.model"
+output_path: str = "fpdoc2vec_umap.png"
+df: pd.DataFrame = load_data(input_path)
+vec: Dict[str, np.ndarray] = make_fp2vector(model_path, df)
+main(input_path, vec, output_path)
+
+#NameDoc2vec visualization
+input_path: str = "10genre_dataset.pkl"
+model_path: str = "namedoc2vec.model"
+output_path: str = "namedoc2vec_umap.png"
+df: pd.DataFrame = load_data(input_path)
+vec: Dict[str, np.ndarray] = make_name2vector(model_path, df)
+main(input_path, vec, output_path)
