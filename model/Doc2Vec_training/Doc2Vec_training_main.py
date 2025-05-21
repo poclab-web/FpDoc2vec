07f529f07e4992ae1ec4c0bab3475efbad34d967
K13abcz <148306813+K13abcz@users.noreply.github.com>
2025-05-21 09:58:49 +0900

    Update Doc2Vec_training_main.py

--- model/Doc2Vec_training/Doc2Vec_training_main.py
+++ model/Doc2Vec_training/Doc2Vec_training_main.py
@@ -15,19 +15,26 @@ param = {"vector_size": 100,
      "seed": 100}
 
 # Building FpDoc2Vec model
-# Generate fingerprints
-df["fp_3_4096"] = generate_morgan_fingerprints(df, 3, 4096)
-finger_list = list(df["fp_3_4096"])
-
 # Example usage - replace with your actual file paths
 input_file = "10genre_dataset.pkl"
 output_model_name = "fpdoc2vec.model"
+finger_list = list(df["fp_3_4096"])
 main_doc2vec(input_file, output_model_name, param, finger_list)
 
 # Building NameDoc2Vec model
 # Extract compound names
 allcompounds = exact_name(df)
-
 # Example usage - replace with your actual file paths
 output_model_name = "namedoc2vec.model"
 main_doc2vec(input_file, output_model_name, param, allcompounds)
+
+# Building FpDoc2Vec model for unseen compounds
+# Generate fingerprints
+input_file = "train_df.pkl"
+with open(input_file, "rb") as f:
+     df = pickle.load(f)
+finger_list = list(df["fp_3_4096"])
+
+# Example usage - replace with your actual file paths
+output_model_name = "fpdoc2vec_unseen.model"
+main_doc2vec(input_file, output_model_name, param, finger_list)
