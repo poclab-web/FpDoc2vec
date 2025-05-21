4a632f5291dc27142a7b761c7fd3cd0b46e8959f
K13abcz <148306813+K13abcz@users.noreply.github.com>
2025-05-21 16:25:19 +0900

    Update shapvalue_to_structure_main.py

--- result/SHAP/shapvalue_to_structure_main.py
+++ result/SHAP/shapvalue_to_structure_main.py
@@ -5,8 +5,7 @@ shap_values_path = "shap_value_ecfp.pkl"
 chemical_data_path = "10genre_dataset.pkl"
 target_molecule = "quercetin" # Please modify according to the purpose.
 
-main(shap_values_path, chemical_data_path=chemical_data_path, target_molecule, fp_radius=3, nBits=4096, scale_factor=1.0)
-
+main(shap_values_path, chemical_data_path, target_molecule, fp_radius=3, nBits=4096, scale_factor=1.0)
 # Example usage with default values
 shap_values_path = "shap_value_doc2vec.pkl"
 chemical_data_path = "10genre_dataset.pkl"
